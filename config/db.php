bdba547fedbd25b5bda373d00180ca89bd5d5085
hexiqiang <376858010@qq.com>
2022-03-31 16:50:09 +0800

    这是后端yii接口

--- config/db.php
+++ config/db.php
@@ -7,10 +7,3 @@ return [
     'password' => 'root',
     'charset' => 'utf8mb4',
 ];
-//return [
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=119.23.106.90;dbname=mj_pro',
-//    'username' => 'root',
-//    'password' => 'root123456',
-//    'charset' => 'utf8mb4',
-//];
